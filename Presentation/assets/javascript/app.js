6fe573053f4db7c75cb0a5db226b852f2db9347b
koshys <samson@samsonkoshy.com>
2018-11-03 10:31:01 -0700

    basic api getData

--- Presentation/assets/javascript/app.js
+++ Presentation/assets/javascript/app.js
@@ -1,4 +1,10 @@
-  // Initialize Firebase
+
+$(document).ready(function() {
+/**
+ * 
+ * GLOBALS
+ */
+// Initialize Firebase
   var config = {
     apiKey: "AIzaSyAaBxgXJBVE_9C8qzRu_ebV1sscAVqiers",
     authDomain: "group-project-4da86.firebaseapp.com",
@@ -9,5 +15,56 @@
   };
   firebase.initializeApp(config);
 
+  /**
+   *  geoIP defaults to empty string
+   *  rangeInMile defaults to 10
+   *  sortField defaults to datetime_local
+   *  sortOrder defaults to desc the other option is asc
+   */
+  var seatgeek = {
+    url : "https://api.seatgeek.com/2/events?client_id=MTM3NTY1NjV8MTU0MTAzNjQ2MC42NA",
+    geoIP : "",
+    rangeInMiles : 10,
+    sortField : "datetime_local",
+    sortOrder : "desc"
+  };
 
-  
+/**
+ * 
+ * END OF GLOBALS
+ */
+
+/**
+ * sample api : https://api.seatgeek.com/2/events?client_id=MTM3NTY1NjV8MTU0MTAzNjQ2MC42NA&geoip=73.241.184.216&range=12mi&sort=datetime_local.desc
+ */
+
+ /**
+  * @param pass the seakgeek obj fully populated
+  * @param geoIP this is the ip from the browser
+  * @param radiusInMiles  this is the radius from the geoip address point.
+  * 
+  * Use the url to search for events that are now and in the future
+  */
+ function getData() {
+
+    $.ajax({
+      url : seatgeek.url + "&geoip=" + seatgeek.geoIP + "&range=" + seatgeek.rangeInMiles + "mi" +
+      "&sort=" + seatgeek.sortField + "." + seatgeek.sortOrder,
+      method : "GET"
+    }).then(function(res){
+
+      return res;
+
+    });
+
+ };
+
+ /**
+  * testing
+  */
+
+
+ seatgeek.geoIP = "73.241.184.21";
+ console.log(getData());
+
+});
